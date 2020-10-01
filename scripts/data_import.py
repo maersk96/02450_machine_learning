ae7a44f3ae87e6b71478e704024a1c930a8fb3ad
maersk96 <jonas.littau@gmail.com>
2020-10-01 17:28:32 +0200

    fix BOM

--- scripts/data_import.py
+++ scripts/data_import.py
@@ -7,10 +7,10 @@ includeCoursePlan = True
 includeMasters = False
 
 # Load the datasets
-df_students = pd.read_csv('../data/students.csv', delimiter=";", decimal=",")
-df_students_course_history = pd.read_csv('../data/students_course_history.csv', delimiter=";")
-df_students_course_plan = pd.read_csv('../data/students_course_plan.csv', delimiter=";")
-df_students_courses_results = pd.read_csv('../data/student_courses_results.csv', delimiter=",")
+df_students = pd.read_csv('../data/students.csv', delimiter=";", decimal=",", encoding="utf-8-sig")
+df_students_course_history = pd.read_csv('../data/students_course_history.csv', delimiter=";", encoding="utf-8-sig")
+df_students_course_plan = pd.read_csv('../data/students_course_plan.csv', delimiter=";", encoding="utf-8-sig")
+df_students_courses_results = pd.read_csv('../data/student_courses_results.csv', delimiter=",", encoding="utf-8-sig")
 
 # Remove masters
 if not includeMasters:
@@ -22,28 +22,22 @@ if includeCoursePlan:
     df_students_courses = df_students_courses.append(df_students_course_plan)
 
 # K-out-of-1 encode the courses
-courses_encoding = pd.crosstab(df_students_courses['student_id'], 
-                            df_students_courses['course'],  
-                               margins = False)  
-
+courses_encoding = pd.crosstab(df_students_courses['student_id'],df_students_courses['course'],margins = False)
 courses_encoding[courses_encoding != 0] = 1
+courses_encoding = courses_encoding.reset_index()
 
 
 # K/D calculator
 KD_courses = pd.crosstab(df_students_courses_results['student_id'],df_students_courses_results['fortolket_res'])
-data_div = KD_courses['bestået']/(KD_courses['bestået'] + KD_courses['ikke bestået'] + KD_courses['forsøg'])
-df_data_div = data_div.to_frame()
-df_data_div.columns=['kd'] 
+data_div = KD_courses['bestået'] / (KD_courses['bestået'] + KD_courses['ikke bestået'] + KD_courses['forsøg'])
+df_data_div = data_div.to_frame().reset_index()
+df_data_div.columns = ['student_id','kd'] 
 
 # Combine students with K-out-of-1 encoded courses
-df_students = df_students.set_index('student_id')
 df_students = pd.merge(df_students, df_data_div, on='student_id', how='left').fillna(1)
 df_students = pd.merge(df_students, courses_encoding, on='student_id',how='left')
-KD_courses = pd.merge(KD_courses, df_data_div, left_index=True, right_index=True)
-
 
 # Extract matrix
-df_students = df_students.reset_index()
 X = df_students.get_values()
 attributeNames = df_students.columns
 N = len(X)
