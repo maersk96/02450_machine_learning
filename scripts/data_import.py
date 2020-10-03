940a4620ffd48b31270afc6f87b02019e5a83def
maersk96 <jonas.littau@gmail.com>
2020-10-03 09:38:42 +0200

    age col

--- scripts/data_import.py
+++ scripts/data_import.py
@@ -20,12 +20,15 @@ if not includeMasters:
 df_students_courses = df_students_course_history
 if includeCoursePlan:
     df_students_courses = df_students_courses.append(df_students_course_plan)
+    
+# Calculate student age
+df_students['age'] = df_students['start_age'] + ((pd.Timestamp('2020-10-01T12') - pd.to_datetime(df_students['start_date'], format="%d-%m-%Y")) / np.timedelta64(1, 'D') / 365)
+
 
 # K-out-of-1 encode the courses
 courses_encoding = pd.crosstab(df_students_courses['student_id'],df_students_courses['course'],margins = False)
 courses_encoding[courses_encoding != 0] = 1
 courses_encoding = courses_encoding.reset_index()
-
 
 # K/D calculator
 KD_courses = pd.crosstab(df_students_courses_results['student_id'],df_students_courses_results['fortolket_res'])
--- scripts/data_plots.py
+++ scripts/data_plots.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 import scipy.stats
 
 min_age = 10
-max_age = 40
+max_age = 35
 mean = np.mean(X[:, 1])
 std = np.std(X[:, 1])
 plt.hist(df_students['age'], color = 'blue', edgecolor = 'black',
-         bins = 60, density=True, range=(min_age, max_age))
+         bins = 100, density=True, range=(min_age, max_age))
 x = np.linspace(min_age, max_age, 1000)
 pdf = scipy.stats.norm.pdf(x,loc=mean,scale=std)
-plot(x,pdf,'.',color='orange', linewidth=15.0)
+plt.plot(x,pdf,'.',color='orange', linewidth=15.0)
 
-plt.title('Age distribution')
+plt.title('Current bachelors age distribution')
 plt.xlabel('age')
 plt.ylabel('density')
 plt.show()
