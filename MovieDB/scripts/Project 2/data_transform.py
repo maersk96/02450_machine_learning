535b62e7c74e027b1a930b6a20d9962be1e425fb
maersk96 <jonas.littau@gmail.com>
2020-11-08 11:48:59 +0100

    KEncode frequency limit

--- MovieDB/scripts/Project 2/data_transform.py
+++ MovieDB/scripts/Project 2/data_transform.py
@@ -1,5 +1,5 @@
 from data_import import *
-import json
+from KEncode import *
 
 #Transform Language - Boolean if(eng) = true else false
 df['original_language'] = df['original_language'] == 'en'; 
@@ -20,32 +20,27 @@ del df['release_date'];
 #Transform Voting average to int
 df['vote_average'] = df['vote_average'].round(0);
 
-#One out of K encoding
-def KEncode(column, key):
-    return df.drop(column, axis=1).join(pd.get_dummies(
-            pd.Series(df[column].apply(json.loads).map(lambda x:[i[key] for i in x])
-                .apply(pd.Series).stack().reset_index(1, drop=True)))
-                .sum(level=0)
-                .add_prefix(column + '_')
-                .fillna(0)
-           )
-    
-df = KEncode('genres', 'name') # +20 features
-#df = KEncode('production_countries', 'name') # +100 features
-#df = KEncode('production_companies', 'name') # +5.000 features
-#df = KEncode('keywords', 'name') # +10.000 features
+
+#Normalize
+## <====>
+
+df = KEncode(df, 'genres', 'name', 50) # +20 features
+#df = KEncode(df, 'production_countries', 'name', 10) # +100 features
+#df = KEncode(df, 'production_companies', 'name', 100) # +5.000 features
+df = KEncode(df, 'keywords', 'name', 100) # +10.000 features
 
 #Remove unnecessary columns
 del df['id'] 
 del df['spoken_languages'] 
 del df['production_companies'] 
 del df['production_countries'] 
-del df['keywords'] 
+#del df['keywords'] 
 del df['homepage'] 
 del df['overview']
 
 ## Prepare for ML
 # Extract X-matrix
+df.fillna(0)
 X = df.get_values()
 
 # Classification variabels
